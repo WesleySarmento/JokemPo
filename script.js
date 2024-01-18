bebbee36dc9cc57c41555422d21146d4dfc76146
WesleySarmento <wesleys.494@gmail.com>
2024-01-17 22:27:27 -0300

    JokemPo Criação

---
+++ script.js
@@ -0,0 +1,45 @@
+/*
+for(let i = 0; i <= 15; i++){
+    console.log(i)
+}
+*/
+
+/*
+function sum(value, value2){
+    console.log (value + value2)
+}
+
+function sub (value, value2){
+    console.log (value - value2)
+}
+
+function div(value, value2){
+    console.log(value / value2)
+}
+
+function mul(value, value2){
+    console.log(value * value2)
+}
+
+sum(5, 6)
+sub(60, 10)
+div(87, 9)
+mul(12, 33)
+
+*/
+
+/*
+    Calcula Desconto:
+    Todos os produtos  acima de R$ 30,00 tem desconto de 10%
+
+*/
+
+const cart = [10, 244, 99, 2, 20, 33, 250]
+
+//função para calcular o preço com desconto 
+
+function desconto(){
+    for(let = i; i< ; i++){
+
+    }
+}
